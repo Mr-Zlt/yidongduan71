70f14420370d48f42c48c17ed5e4364bf9184fa7
zhaoleitao <zhaoleitao@itcast.cn>
2019-07-09 15:01:55 +0800

    登陆页面登陆

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -21,24 +21,39 @@
             />
         </van-cell-group>
         <div class="login-btn">
-            <van-button class="btn" type="info">登陆</van-button>
+            <van-button class="btn" type="info" @click.prevent="handleLogin">登陆</van-button>
         </div>
     </form>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'LoginIndex',
   data () {
     return {
       user: {
-        mobile: '',
-        code: ''
+        mobile: '18801185985',
+        code: '123456'
+      }
+    }
+  },
+  methods: {
+    async handleLogin () {
+      try {
+        const res = await axios({
+          method: 'POST',
+          url: 'http://toutiao.course.itcast.cn/app/v1_0/authorizations',
+          data: this.user
+        })
+        console.log(res)
+        this.$router.push({ name: 'Home' })
+      } catch (error) {
+        console.log(error)
       }
     }
   }
-
 }
 </script>
 
